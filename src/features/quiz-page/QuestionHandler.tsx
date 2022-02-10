187ee35fa844e0a9568b08519126d4a156945a0b
Marcus Stamström <marcus.stamstrom@gmail.com>
2022-02-10 17:13:15 +0100

    adding styling for answers

--- src/features/quiz-page/QuestionHandler.tsx
+++ src/features/quiz-page/QuestionHandler.tsx
@@ -1,16 +1,31 @@
-import { Question } from "../../common/requests/quizRequest";
+import clsx from "clsx";
+import { Answer, Question } from "../../common/requests/quizRequest";
 import styles from "./QuestionHandler.module.css";
 interface Props {
   question: Question;
+  userAnswer: Answer | undefined;
+  onClick: (answer: Answer) => void;
 }
 
-export const QuestionHandler = ({ question }: Props) => {
+export const QuestionHandler = ({ question, userAnswer, onClick }: Props) => {
   return (
     <div>
       <span>{question.question}</span>
       <div className={styles.answerContainer}>
         {question.answers.map((answer, index) => (
-          <button className={styles.answer}>
+          <button
+            disabled={userAnswer !== undefined}
+            className={clsx([
+              styles.answer,
+              userAnswer?.index === index && styles.selectedAnswer,
+              userAnswer && answer.isCorrectAnswer && styles.correctAnswer,
+              userAnswer &&
+                !userAnswer?.isCorrectAnswer &&
+                userAnswer?.index === index &&
+                styles.wrongAnswer,
+            ])}
+            onClick={() => onClick(answer)}
+          >
             <span className={styles.index}>{index + 1}</span>
             <span className={styles.answerText}>{answer.answer}</span>
           </button>
--- src/features/quiz-page/QuizPage.tsx
+++ src/features/quiz-page/QuizPage.tsx
@@ -1,10 +1,44 @@
-import { Question } from "../../common/requests/quizRequest";
+import { useState } from "react";
+import { Button } from "../../common/components/Button";
+import { Answer, Question } from "../../common/requests/quizRequest";
 import { QuestionHandler } from "./QuestionHandler";
 
 interface Props {
   questions: Question[];
 }
 
+interface UserAnswer {
+  questionText: string;
+  answer: Answer;
+}
+
 export const QuizPage = ({ questions }: Props) => {
-  return <QuestionHandler question={questions[0]} />;
+  const [answers, setAnswers] = useState<UserAnswer[]>([]);
+  const [currentQuestionIndex, setCurrentQuestionIndex] = useState(0);
+
+  const onAnswer = (answer: Answer) => {
+    setAnswers([
+      ...answers,
+      { questionText: questions[currentQuestionIndex].question, answer },
+    ]);
+  };
+
+  const moveToNextQuestion = () => {
+    setCurrentQuestionIndex(currentQuestionIndex + 1);
+  };
+  return (
+    <div>
+      <QuestionHandler
+        question={questions[currentQuestionIndex]}
+        onClick={onAnswer}
+        userAnswer={answers[currentQuestionIndex]?.answer}
+      />
+      <Button
+        disabled={answers[currentQuestionIndex] === undefined}
+        onClick={() => moveToNextQuestion()}
+      >
+        Next question
+      </Button>
+    </div>
+  );
 };
